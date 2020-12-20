5b82f6bd8dbb94c1338eccddea437692ca22cd6f
repk1124 <34098908+repk1124@users.noreply.github.com>
2020-12-20 16:04:45 +0800

    Update speech.py

--- speech.py
+++ speech.py
@@ -7,32 +7,32 @@ cap=cv2.VideoCapture(0)
 r = sr.Recognizer()
 r.energy_threshold = 4000
 
-def camera():
+def camera(): #相機Live畫面
     print("Camera Start")
     while(cap.isOpened()):
         ret ,frame = cap.read()
         cv2.imshow("capture", frame)
         cv2.waitKey(1)
         
-def speech():
+def speech(): #接收語音指令進行拍照與看照片
     i=0
     img = 0
     while(cap.isOpened()):
         ret ,frame = cap.read()
-        #my_stt = r.recognize_google(audio,language="en-US")
+        my_stt = r.recognize_google(audio,language="en-US")
         print("Speech Start")
-        my_stt = input()
+        #my_stt = input()
         if my_stt == "take a photo":
             cv2.imwrite('C:\\Users\\repk1\\Desktop\\speech\\'+str(i)+'.jpg',frame)
             img = cv2.imread('C:\\Users\\repk1\\Desktop\\speech\\'+str(i)+'.jpg')
             i+=1
+            print(my_stt)
             continue
         elif my_stt == "show the photo":
             cv2.imshow("img",img)
             cv2.waitKey (0)
+            print(my_stt)
             continue
-        print(img)
-        print(my_stt)
     
 while(1):
     with sr.Microphone() as source:
@@ -40,7 +40,7 @@ while(1):
     try:
         threads = [threading.Thread(target=camera),threading.Thread(target=speech)]
         for t in threads:
-            t.start()
+            t.start()  #啟動多執行續執行倆def
     except sr.UnknownValueError:
         print("Could Not Understand !")
     except sr.RequestError as e:
